ceff3cdb384e92a0b5d4b9e91ff4f668f43b97de
SdtElectronics <meiyang423@Gmail.com>
2021-01-01 15:34:24 +0800

    update utils.sh

--- utils.sh
+++ utils.sh
@@ -1,2 +1,8 @@
 #Forward serial tty console to display
 script -f /dev/tty0
+
+#mountdebugfs
+mount -t debugfs none /sys/kernel/debug
+
+#mount tf card and permit running executables
+mount -t vfat -o umask=0000 /dev/block/mmcblk0 /mnt/sdcard
